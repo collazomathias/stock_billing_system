c5c725805946917790c535ae2f82de269be0011c
collazomathias <mathias.collazo.episcopo@gmail.com>
2022-03-30 11:54:11 -0300

    First commit. Added backend and frontend structured correctly.

---
+++ README.md
@@ -0,0 +1,8 @@
+# STOCK & BILLING SYSTEM
+
+#### Frontend 
+- ReactJS
+#### Backend
+- Java with SpringBoot
+#### DB
+- MongoDB with Mongo Atlas
---
+++ backend/src/main/java/uy/com/sofka/stockbilling/StockbillingApplication.java
@@ -0,0 +1,13 @@
+package uy.com.sofka.stockbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockbillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockbillingApplication.class, args);
+	}
+
+}
